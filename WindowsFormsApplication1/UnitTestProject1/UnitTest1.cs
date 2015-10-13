9ed603f157955390e86f41335a8e25614755acb5
Emilovic <emilandersen92@hotmail.com>
2015-10-13 13:24:12 +0200

    Test builder

---
+++ WindowsFormsApplication1/UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void SuccesTest()
+        {
+            Assert.IsTrue(true);
+        }
+        public void FailedTest()
+        {
+            Assert.IsTrue(false);
+        }
+    }
+}
--- WindowsFormsApplication1/WindowsFormsApplication1/Form1.cs
+++ WindowsFormsApplication1/WindowsFormsApplication1/Form1.cs
@@ -16,5 +16,10 @@ namespace WindowsFormsApplication1
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
     }
 }
