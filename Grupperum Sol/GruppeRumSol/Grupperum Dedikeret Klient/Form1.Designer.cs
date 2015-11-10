6ca1ddef63137a6e923a7fa24376c80f945ce4aa
David Kolind <dmkolind@nokgodtnok.dk>
2015-11-10 10:29:44 +0100

    Lavet GruppeRum Solution

---
+++ Grupperum Sol/GruppeRumSol/Grupperum Dedikeret Klient/Form1.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace Grupperum_Dedikeret_Klient
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_CreateRoom = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btn_CreateRoom
+            // 
+            this.btn_CreateRoom.Location = new System.Drawing.Point(87, 118);
+            this.btn_CreateRoom.Name = "btn_CreateRoom";
+            this.btn_CreateRoom.Size = new System.Drawing.Size(75, 23);
+            this.btn_CreateRoom.TabIndex = 0;
+            this.btn_CreateRoom.Text = "Opret lokale";
+            this.btn_CreateRoom.UseVisualStyleBackColor = true;
+            this.btn_CreateRoom.Click += new System.EventHandler(this.btn_CreateRoom_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.btn_CreateRoom);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_CreateRoom;
+    }
+}
+
