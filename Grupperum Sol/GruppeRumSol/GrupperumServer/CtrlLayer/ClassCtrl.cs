a9a87c595a3ea1652bfb6f3f326d941cc0a84727
Mark Hjorth Sørensen <MarkHjorthSorensen@gmail.com>
2015-11-10 13:26:12 +0100

    CtrlLayer

---
+++ Grupperum Sol/GruppeRumSol/GrupperumServer/CtrlLayer/ClassCtrl.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GrupperumServer.ModelLayer;
+
+namespace GrupperumServer.CtrlLayer
+{
+    class ClassCtrl
+    {
+        public ClassCtrl()
+        {
+        }
+        
+        public void addStudentToClass(Student student, Class clas)
+        {
+            clas.addStudent(student);
+        }
+    }
+}
