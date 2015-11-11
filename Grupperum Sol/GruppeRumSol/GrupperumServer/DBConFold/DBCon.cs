e8dadbb71d0ccfc19c5fca8bb7144501d15653f6
Emilovic <Emilandersen1992@gmail.com>
2015-11-11 12:07:19 +0100

    DBCon merge fix

--- Grupperum Sol/GruppeRumSol/GrupperumServer/DBConFold/DBCon.cs
+++ Grupperum Sol/GruppeRumSol/GrupperumServer/DBConFold/DBCon.cs
@@ -11,7 +11,7 @@ namespace GrupperumServer.DBConFold
     public class DBCon
     {
         SqlConnection con;
-        public void connect()
+        public void connect();
         string conString = "user id=dmaa0914_3Sem_2_Grupperum;" +
                                    "password=IsAllowed;server=kraka.ucn.dk;" +
                                    "database=dmaa0914_3Sem_2_Grupperum; " +
@@ -19,15 +19,6 @@ namespace GrupperumServer.DBConFold
 
         public SqlDataReader ExecuteString(string command)
         {
-
-            con = new SqlConnection(conString);
-
-        }
-    public void createGroupRoom()
-        {
-            connect();
-            con.Open();
-            SqlCommand sc = new SqlCommand("insert into GroupRoom DEFAULT VALUES;", con);
             SqlConnection con = new SqlConnection(conString);
 
             con.Open();
