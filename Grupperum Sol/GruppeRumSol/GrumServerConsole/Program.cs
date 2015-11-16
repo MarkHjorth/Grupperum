f161dab4cfcc0000bcabd78b45fdcb929d2e3965
David Kolind <dmkolind@nokgodtnok.dk>
2015-11-16 12:55:06 +0100

    Fixed something that was not WCF

---
+++ Grupperum Sol/GruppeRumSol/GrumServerConsole/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ServiceModel;
+using System.Threading.Tasks;
+using WcfGrumService;
+
+namespace GrumServerConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (ServiceHost serviceHost = new ServiceHost(typeof(GrumService)))
+            {
+                serviceHost.Open();
+                DisplayHostInfo(serviceHost);
+                Console.WriteLine("The service is ready.");
+                Console.WriteLine("Press the Enter key to terminate service.");
+                Console.ReadLine();
+            }
+        }
+
+        static void DisplayHostInfo(ServiceHost host)
+        {
+            Console.WriteLine();
+            Console.WriteLine("****** Host Info *******");
+            foreach (System.ServiceModel.Description.ServiceEndpoint se
+            in host.Description.Endpoints)
+            {
+                Console.WriteLine("Address: {0}", se.Address);
+                Console.WriteLine("Binding: {0}", se.Binding.Name);
+                Console.WriteLine("Contract: {0}", se.Contract.Name);
+                Console.WriteLine();
+            }
+            Console.WriteLine("************************");
+        }
+    }
+}
--- Grupperum Sol/GruppeRumSol/GrupperumServer/ModelLayer/Student.cs
+++ Grupperum Sol/GruppeRumSol/GrupperumServer/ModelLayer/Student.cs
@@ -1,11 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Serialization;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace GrupperumServer.ModelLayer
 {
+    [DataContract]
     public class Student
     {
         private int id;
