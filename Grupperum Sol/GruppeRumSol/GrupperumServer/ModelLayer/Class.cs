0966adbce16020ca8ff39221d18222a57e485ddc
David Kolind <dmkolind@nokgodtnok.dk>
2015-11-16 10:01:25 +0100

    WCF fix (Overrule)

--- Grupperum Sol/GruppeRumSol/GrupperumServer/ModelLayer/Class.cs
+++ Grupperum Sol/GruppeRumSol/GrupperumServer/ModelLayer/Class.cs
@@ -3,12 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Runtime.Serialization;
 
 namespace GrupperumServer.ModelLayer
 {
+    [DataContract]
     public class Class
     {
+        [DataMember]
         private int id;
+        [DataMember]
         private List<Student> studentList = new List<Student>();
         
         public Class(int id)
@@ -18,15 +22,17 @@ namespace GrupperumServer.ModelLayer
 
         public Class(int tempId, string tempName)
         {
-              
+            this.id = tempId;
         }
-
+        
         public void addStudent(Student student)
         {
             this.studentList.Add(student);
         }
 
+        [DataMember]
         public int Id { get; set; }
+        [DataMember]
         public List<Student> StudentList { get; set; }
     }
 }
