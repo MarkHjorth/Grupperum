a79eb7a4e83e2f60a5f895b59f19f62b0cc20fd9
David Kolind <dmkolind@nokgodtnok.dk>
2015-11-23 15:58:18 +0100

    Følger nu MVC konventioner Bruger model i MVC (CreateGroupModel)

---
+++ Grupperum Sol/GruppeRumSol/Grupperum WebSite Klient/Models/Home/CreateGroupModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Grupperum_Website_Klient.Models.Home
+{
+    public class CreateGroupModel
+    {
+        public string Name { get; set; }
+
+        public List<Student> Students { get; set; }
+
+    }
+}
