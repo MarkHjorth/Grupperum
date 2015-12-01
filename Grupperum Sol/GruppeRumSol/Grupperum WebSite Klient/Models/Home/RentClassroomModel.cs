583df02d21b908e5e2291709f30fe8bbf7548051
Emilovic <emilandersen1992@gmail.com>
2015-12-01 20:40:05 +0100

    MVC page for Grouplist

--- Grupperum Sol/GruppeRumSol/Grupperum WebSite Klient/Models/Home/RentClassroomModel.cs
+++ Grupperum Sol/GruppeRumSol/Grupperum WebSite Klient/Models/Home/RentClassroomModel.cs
@@ -7,9 +7,13 @@ namespace Grupperum_Website_Klient.Models.Home
 {
     public class RentClassroomModel
     {
+        public String DateStart { get; set; }
+        public String DateFinish { get; set; }
+
         public int GrId { get; set; }
         public string GrIdStr { get; set; }
         public int GrSize { get; set; }
+        public bool ClassRoom { get; set; }
         public bool Whiteboard { get; set; }
         public bool Monitor { get; set; }
         public bool Projector { get; set; }
@@ -17,7 +21,8 @@ namespace Grupperum_Website_Klient.Models.Home
 
         public RentClassroomModel()
         {
-
+            DateStart = DateTime.Today.Date.ToString("dd/MM/yyyy");
+            DateFinish = DateTime.Today.Date.ToString("dd/MM/yyyy");
         }
     }
 }
