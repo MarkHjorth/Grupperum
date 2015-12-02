580b0cf7b8818bd06e38802d61de2eaa9cdddd3b
Mark Hjorth Sørensen <MarkHjorthSorensen@gmail.com>
2015-12-02 12:38:23 +0100

    RentCtrl added, which can rent a groupRoom. RentCtrl tests to see if groupRoom is still free and then rents it.

---
+++ Grupperum Sol/GruppeRumSol/GrupperumServer/CtrlLayer/RentCtrl.cs
@@ -0,0 +1,30 @@
+﻿using GrupperumServer.DBConFold;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GrupperumServer.CtrlLayer
+{
+    public class RentCtrl
+    {
+        DBCtrl dbCtrl = new DBCtrl();
+
+        public bool RentGroupRoom(int grouproomId, int groupId, DateTime dateStart, DateTime dateEnd)
+        {
+            bool isRented = false;
+            if (TestGroupRoom(grouproomId, dateStart, dateEnd))
+            {
+                isRented = dbCtrl.RentGroupRoom(grouproomId, groupId, dateStart, dateEnd);
+            }
+            return isRented;
+        }
+
+        public bool TestGroupRoom(int grouproomId, DateTime dateStart, DateTime dateEnd)
+        {
+            return dbCtrl.TestGroupRoom(grouproomId, dateStart, dateEnd);
+        }
+
+    }
+}
