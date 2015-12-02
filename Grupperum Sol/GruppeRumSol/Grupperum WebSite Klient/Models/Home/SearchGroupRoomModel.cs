5c049d7afcd521a10009127b51554474db5497c6
Emilovic <emilandersen1992@gmail.com>
2015-12-02 09:35:14 +0100

    Det fint

---
+++ Grupperum Sol/GruppeRumSol/Grupperum WebSite Klient/Models/Home/SearchGroupRoomModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Grupperum_Website_Klient.Models.Home
+{
+    public class SearchGroupRoomModel
+    {
+        public String DateStart { get; set; }
+        public String DateFinish { get; set; }
+
+        public int GrId { get; set; }
+        public string GrIdStr { get; set; }
+        public int GrSize { get; set; }
+        public bool ClassRoom { get; set; }
+        public bool Whiteboard { get; set; }
+        public bool Monitor { get; set; }
+        public bool Projector { get; set; }
+        public Request request { get; set; }
+
+        public SearchGroupRoomModel()
+        {
+            DateStart = DateTime.Today.Date.ToString("dd/MM/yyyy");
+            DateFinish = DateTime.Today.Date.ToString("dd/MM/yyyy");
+        }
+    }
+}
