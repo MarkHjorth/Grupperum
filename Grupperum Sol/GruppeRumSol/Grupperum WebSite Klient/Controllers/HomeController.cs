ea88cb7e9e6e677fe4bbb1fc43e71ba4c991a7e1
Emilovic <emilandersen1992@gmail.com>
2015-12-03 09:37:19 +0100

    DET VIRKER Vi kan få listen frem i webGui

--- Grupperum Sol/GruppeRumSol/Grupperum WebSite Klient/Controllers/HomeController.cs
+++ Grupperum Sol/GruppeRumSol/Grupperum WebSite Klient/Controllers/HomeController.cs
@@ -77,44 +77,54 @@ namespace Grupperum_Website_Klient.Controllers
         [HttpPost]
         public ActionResult Rent(RentModel formModel)
         {
+            RentModel model = new RentModel();
+
+            DateTime ds = formModel.DateStart;
+            DateTime df = formModel.DateFinish;
+
+            bool cr = formModel.request.ClassRoom;
+            string idS = formModel.GrIdStr;
+            int id = Int32.Parse(idS);
+            int si = formModel.request.GrSize;
+            bool wh = formModel.request.Whiteboard;
+            bool mon = formModel.request.Monitor;
+            bool pr = formModel.request.Projector;
+            TempData["ds"] = ds;
+            TempData["df"] = df;
+            TempData["si"] = si;
+            TempData["wh"] = wh;
+            TempData["mon"] = mon;
+            
+            return Redirect("Grouproom");
+        }
+
+        [HttpGet]
+        public ActionResult Grouproom()
+        {
             GroupRoomModel model = new GroupRoomModel();
+
             using (GrumServiceClient client = new GrumServiceClient())
             {
-                DateTime ds = formModel.DateStart;
-                DateTime df = formModel.DateFinish;
-
-                bool cr = formModel.request.ClassRoom;
-                string idS = formModel.GrIdStr;
-                int id = Int32.Parse(idS);
-                int si = formModel.request.GrSize;
-                bool wh = formModel.request.Whiteboard;
-                bool mon = formModel.request.Monitor;
-                bool pr = formModel.request.Projector;
-
+                DateTime ds = (DateTime)TempData["ds"];
+                DateTime df = (DateTime)TempData["df"];
+                int si = (int)TempData["si"];
+                bool wh = (bool)TempData["wh"];
+                bool mon = (bool)TempData["mon"];
 
                 model.GroupRoomList = client.GetGroupRoomList(ds, df, si, wh, mon)
                      .Select(gr => new Models.Home.GroupRoom() { GroupId = gr.Id, GroupName = gr.Name })
                      .ToList();
-            }
             
-            if (model.GroupRoomList.Count == 0 || model.GroupRoomList.Count == -1)
-            {
-                model.DisplayList = false;
+                if (model.GroupRoomList.Count == 0 || model.GroupRoomList.Count == -1)
+                {
+                    model.DisplayList = false;
+                }
             }
-
-            return Redirect("Grouproom"(model));
+            return View(model);
         }
 
-        [HttpGet]
-        public ActionResult Grouproom(GroupRoomModel model)
-        {
-            using (GrumServiceClient client = new GrumServiceClient())
-            { 
-            
-            } return View(model);
-        }
         [HttpPost]
-        public ActionResult Grouproom()
+        public ActionResult Grouproom(GroupRoomModel formModel)
         {
             return Redirect("index");
         }
