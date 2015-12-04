15b1d7fb2979c5a0ac28cedf4a471f48ae0350b7
Mark Hjorth Sørensen <MarkHjorthSorensen@gmail.com>
2015-12-04 12:33:42 +0100

    Circular dependency fix: Mail now takes 2 int lists: reqListGroupId and reqListClassRoomId

--- Grupperum Sol/GruppeRumSol/MailSystem/Program.cs
+++ Grupperum Sol/GruppeRumSol/MailSystem/Program.cs
@@ -3,28 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using GrupperumServer.ModelLayer;
 
 namespace MailSystem
 {
     class Mail
     {
-
-
-
-        public Mail(List<RequestMatch> reqList, List<int> failList)
+        public Mail(List<int> reqListGroupId, List<int> reqListClassRoomId, List<int> failList)
         {
-            PrintRent(reqList, failList);
+            PrintRent(reqListGroupId, reqListClassRoomId, failList);
         }
 
-        public void PrintRent(List<RequestMatch> reqList, List<int> failList)
+        public void PrintRent(List<int> reqListGroupId, List<int> reqListClassRoomId, List<int> failList)
         {
-            int length = reqList.Count();
+            int length = reqListGroupId.Count();
 
             for (int i = 0; i < length; i++)
             {
                 string print = string.Format("Mail sedt til GruppeID {0}. " +
-                "Gruppen har fået lokaleID {1}", reqList[i].GroupId, reqList[i].ClassroomId);
+                "Gruppen har fået lokaleID {1}", reqListGroupId[i], reqListClassRoomId[i]);
                 Console.WriteLine(print);
             }
 
