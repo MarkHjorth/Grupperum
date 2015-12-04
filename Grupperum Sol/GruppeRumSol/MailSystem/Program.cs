b1143a1f47d402dbce414d524118b4188991cd8f
Mark Hjorth Sørensen <MarkHjorthSorensen@gmail.com>
2015-12-04 12:23:11 +0100

    Mail now also takes List<int> failList. The groups whom didn't get a classroom.

--- Grupperum Sol/GruppeRumSol/MailSystem/Program.cs
+++ Grupperum Sol/GruppeRumSol/MailSystem/Program.cs
@@ -10,21 +10,33 @@ namespace MailSystem
     class Mail
     {
 
-        public Mail(List<RequestMatch> reqList)
+
+
+        public Mail(List<RequestMatch> reqList, List<int> failList)
         {
-            PrintRent(reqList);
+            PrintRent(reqList, failList);
         }
 
-        public void PrintRent(List<RequestMatch> reqList)
+        public void PrintRent(List<RequestMatch> reqList, List<int> failList)
         {
             int length = reqList.Count();
 
             for (int i = 0; i < length; i++)
             {
-                string print = string.Format("Mail sedt til {0}. " +
-                "Gruppen har fået lokale {1}", reqList[i].GroupId, reqList[i].ClassroomId);
+                string print = string.Format("Mail sedt til GruppeID {0}. " +
+                "Gruppen har fået lokaleID {1}", reqList[i].GroupId, reqList[i].ClassroomId);
                 Console.WriteLine(print);
             }
+
+            length = failList.Count();
+
+            for (int i = 0; i < length; i++)
+            {
+                string print = string.Format("Mail sedt til GruppeID {0}. " +
+                "Gruppen fik desværre ikke et lokale.", failList[i]);
+                Console.WriteLine(print);
+            }
+
             Console.ReadKey();
         }
     }
